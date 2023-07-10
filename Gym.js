76d434f2f8d8d3073c1328ec7823d741c379c929
NabeelaPasha <99131968+Nabeela-Pasha@users.noreply.github.com>
2023-07-10 12:58:22 +0530

    Update Gym.js

--- Gym.js
+++ Gym.js
@@ -6,7 +6,7 @@ const bodyparser=require('body-parser');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/nabbu');
+  await mongoose.connect('mongodb://127.0.0.1:27017');
   console.log("we are connected to gym website!");
 }
 
